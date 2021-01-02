50f014015f0cf8ab8a41649f7481b2c312fc6b85
julianavbr <julianav.br@gmail.com>
2021-01-02 18:39:32 +0000

    teste05

--- app.js
+++ app.js
@@ -103,7 +103,9 @@ router.post('/post/delete', function (req, res) {
     res.redirect('back');
 
 });
-
+server.listen(process.env.PORT || 5501, process.env.IP || "0.0.0.0", function () {
+    var addr = server.address();
+    console.log("Server listnening at", addr.address + ":" + addr.port);
 server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function () {
     var addr = server.address();
     console.log("Server listnening at", addr.address + ":" + addr.port);
